c6360d6f682c120335b02de72cc39081530ca3a8
wosch231 <77884030+wosch231@users.noreply.github.com>
2023-01-19 19:41:25 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,20 +1,43 @@
-input.onButtonPressed(Button.A, function () {
-    oled_z.oled_Line1(64, 0, 38, 60)
-    oled_z.oled_Line1(38, 60, 94, 24)
-    oled_z.oled_Line1(94, 24, 34, 24)
-    oled_z.oled_Line1(34, 24, 84, 64)
-    oled_z.oled_Line1(84, 64, 64, 0)
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+	
 })
+input.onButtonPressed(Button.A, function () {
+    oled_z.oled_Line1(64, 0, 82, 57)
+    oled_z.oled_Line1(82, 57, 34, 22)
+    oled_z.oled_Line1(34, 22, 94, 22)
+    oled_z.oled_Line1(94, 22, 46, 57)
+    oled_z.oled_Line1(46, 57, 64, 0)
+})
+function seri (win: number, r: number, xm: number, ym: number, x: number[], y: number[]) {
+    serial.writeValue("win", win)
+    serial.writeValue("x", Math.floor(xm + Math.sin(win) * r))
+    serial.writeValue("y", Math.floor(ym - Math.cos(win) * r))
+    serial.writeValue("x0", x[0])
+    serial.writeValue("y0", y[0])
+    serial.writeValue("x1", x[1])
+    serial.writeValue("y1", y[1])
+}
 input.onButtonPressed(Button.AB, function () {
-    kitronik_VIEW128x64.drawRect(60, 30, 34, 0)
+	
 })
 input.onButtonPressed(Button.B, function () {
     oled_z.Kreis(64, 32, 31)
 })
+let win = 0
+let ym = 0
+let xm = 0
+let r = 0
 basic.showIcon(IconNames.Surprised)
 kitronik_VIEW128x64.clear()
-let win = 360 / 5
-let z = [0, 1, 0]
-for (let Index = 0; Index <= 4; Index++) {
-    z.insertAt(Index, 11)
-}
+r = 31
+xm = 64
+ym = 32
+let w = 360 / 5 * 2
+win = w - 90
+let x = [64]
+let y = [1]
+seri(win, r, xm, ym, x, y)
+x.push(Math.floor(xm - Math.sin(win) * r))
+y.push(Math.floor(ym - Math.cos(win) * r))
+seri(win, r, xm, ym, x, y)
+oled_z.oled_Line1(x[0], y[0], x[1], y[1])
