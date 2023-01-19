59b4e614c4f50946fd36210b4c4ecab1bdece707
wosch231 <77884030+wosch231@users.noreply.github.com>
2023-01-19 19:52:09 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -10,12 +10,10 @@ input.onButtonPressed(Button.A, function () {
 })
 function seri (win: number, r: number, xm: number, ym: number, x: number[], y: number[]) {
     serial.writeValue("win", win)
-    serial.writeValue("x", Math.floor(xm + Math.sin(win) * r))
-    serial.writeValue("y", Math.floor(ym - Math.cos(win) * r))
-    serial.writeValue("x0", x[0])
-    serial.writeValue("y0", y[0])
-    serial.writeValue("x1", x[1])
-    serial.writeValue("y1", y[1])
+    for (let Index = 0; Index <= x.length - 1; Index++) {
+        serial.writeValue("x", x[Index])
+        serial.writeValue("y", y[Index])
+    }
 }
 input.onButtonPressed(Button.AB, function () {
 	
@@ -24,19 +22,17 @@ input.onButtonPressed(Button.B, function () {
     oled_z.Kreis(64, 32, 31)
 })
 let win = 0
-let ym = 0
-let xm = 0
-let r = 0
 basic.showIcon(IconNames.Surprised)
 kitronik_VIEW128x64.clear()
-r = 31
-xm = 64
-ym = 32
+let r = 31
+let xm = 64
+let ym = 32
 let w = 360 / 5 * 2
-win = w - 90
+win = 0
 let x = [64]
 let y = [1]
 seri(win, r, xm, ym, x, y)
+win = w - 90
 x.push(Math.floor(xm - Math.sin(win) * r))
 y.push(Math.floor(ym - Math.cos(win) * r))
 seri(win, r, xm, ym, x, y)
