14368e5ea61f7f4896c4e4760d46dc35f815c5dc
wosch231 <77884030+wosch231@users.noreply.github.com>
2023-01-20 20:33:42 +0100

    Aktualisiere main.blocks, main.ts, custom.ts

--- custom.ts
+++ custom.ts
@@ -10,23 +10,134 @@
 
 //% weight=100 color=#0fbc11 icon="\uf14b" block="128x64 Display_z"
 namespace oled_z {
+    let zpar: number[] = [];
+    zpar[0] =   1;   // Maßstab x-Achse
+    zpar[1] =   1;   // Maßstab y-Achse
+    zpar[2] =   0;   // Nullpunt x-Achse
+    zpar[3] =   0;   // Nullpunt y-Achse
+    zpar[4] =   0;   // Drehwinkel zu x-Achse
+    zpar[5] =   0;   // Drehwinkel zu y-Achse
+    zpar[6] = 1.1;   // Strichart . Werkzeugnummer (Strichbreite)
+
+    let lx: number
+    let ly: number
+    let ix:number
+    let iy:number
+
+    function grabo(w:any){
+        let grabo=w*0.017453292
+        return grabo
+    }
+    
+    function bogra(w:any) {
+        let bogra = w * 57.29578
+        return bogra
+    }
+
+    function zkoord(x:number, y:number){
+        let xt=grabo(zpar[4])
+        let yt=grabo(zpar[5])
+        let c1=Math.cos(xt)
+        let c2=Math.cos(yt)
+        let s1=Math.sin(xt)
+        let s2=Math.sin(yt)
+        xt = x * zpar[0]
+        yt = y * zpar[1]
+        ix=1*(xt*c1-yt*s2+zpar[2])
+        iy=1*(yt*c2+xt*s1+zpar[3])
+        return
+    }
+
+    /**
+     * Ändert einen Zeichnungsparameter nrpar auf den Wert wert
+     */
+    //% blockId=oled_zparneu
+    //% block="Setze den Zeichnungsparamerter nrpar %nrpar auf wert %wert"
+    //% inlineInputMode=inline
+    //% nrpar.min=0 nrpar.max=6
+    //% wert.min=0 wert.max=6
+    //% weight=100 blockGap=8
+    export function zparneu(nrpar:number, wert:number){
+        if (nrpar <= 0 && nrpar >6) return
+        zpar[nrpar]=wert
+    }
+ 
+    /**
+     * Ändert den Maßstab für die x-Achse und die y-Achse
+     */
+    //% blockId=oled_zmassb
+    //% block="Setze den Maßstab auf xm %XM und ym %YM"
+    //% inlineInputMode=inline
+    //% weight=90 blockGap=8
+    export function zmassb(xm:number, ym:number){
+        zparneu(0,xm)
+        zparneu(1,ym)
+    }
+    
+    /**
+     * Ändert den Nullpunkt xn %xn und yn %yn
+     */
+    //% blockId=oled_znullp
+    //% block="Setze den Nullpunkt auf xn %xn und yn %yn"
+    //% inlineInputMode=inline
+    //% weight=80 blockGap=8
+    export function znullp(xn:number, yn:number) {
+        zparneu(2, xn)
+        zparneu(3, yn)
+    }
+
+    /**
+     * Ändert den Drehwinkel der x-Achse %xw und y-Achse %yw
+     */
+    //% blockId=oled_zdrehw
+    //% block="Setze den Drehwinkel für x-Achse %xw und y-Achse %yw"
+    //% inlineInputMode=inline
+    //% weight=70 blockGap=8
+    export function zdrehw(xw:number, yw:number) {
+        zparneu(4, xw)
+        zparneu(5, yw)
+    }
+
+    /**
+     * Zeichnet ein Polygon mit n Punkten in den Feldern x und y auf dem oled-display
+     */
+    //% blockId=oled_zlinie
+    //% block="Zeichne Polygon mit %n Punkten im Feld x %x und y %y"
+    //% inlineInputMode=inline
+    //% weight=50 blockGap=8
+    export function zlinie(n:number){
+        if (n<1) return
+        let xx=xarr[0]
+        let yy=yarr[0]
+        zkoord(xx, yy)
+        lx = ix
+        ly = iy
+        for (let i = 1; i < n; i++) {
+            xx = xarr[i]
+            yy = yarr[i]
+            zkoord(xx, yy)
+            oled_Line1(lx,ly,ix,iy)
+            lx=xx
+            ly=yy
+        }
+    }
+
     /**
      * Zeichnet eine Linie auf dem oled-display
-     * @param x0 Start (0-16)
-     * @param y0 Start (0-6)
-     * @param x1 Ende (0-16)
-     * @param y1 Ende (0-6)
-     * @param brightness (0-255)
+     * @param x0 Start (0-128)
+     * @param y0 Start (0-64)
+     * @param x1 Ende (0-128)
+     * @param y1 Ende (0-64)
      */
     //% blockId=oled_Line1
     //% block="Zeichne Linie von x0 %x0 y0 %y0 nach x1 %x1 y1 %y1"
     //% inlineInputMode=inline
-    //% x0.min=0 x0.max=16
-    //% y0.min=0 x0.max=6
-    //% x1.min=0 x1.max=16
-    //% y1.min=0 y1.max=6
-    //% weight=70 blockGap=8
-    export function oled_Line1(x0: number, y0: number, x1: number, y1: number): void {
+    //% x0.min=0 x0.max=128
+    //% y0.min=0 x0.max=64
+    //% x1.min=0 x1.max=128
+    //% y1.min=0 y1.max=64
+    //% weight=60 blockGap=8
+    export function oled_Line1(x0: number, y0: number, x1: number, y1: number){
         let dx: number
         let dy: number
         let sx: number
@@ -68,11 +179,17 @@ namespace oled_z {
         }
     }
 
-    //% blockId=Kreis
+    /**
+     * Zeichnet einen Kreis auf dem oled-display
+     * @param Mittelpunkt xm (0-128)
+     * @param Mittelpunkt ym (0-64)
+     * @param r Radius (0-128)
+     */
+    //% blockId=oled_Kreis
     //% block="Zeichne Kreis mit (xm,ym)%xm%ym und Radius%R"
     //% inlineInputMode=inline
-    //% weight=60 blockGap=8
-    export function Kreis(xm: number, ym: number, r: number) {
+    //% weight=40 blockGap=8
+    export function oled_Kreis(xm: number, ym: number, r: number) {
         let f = 1 - r;
         let ddF_x = 0;
         let ddF_y = -2 * r;
--- main.ts
+++ main.ts
@@ -6,33 +6,29 @@ input.onButtonPressed(Button.A, function () {
     oled_z.oled_Line1(94, 22, 46, 57)
     oled_z.oled_Line1(46, 57, 64, 0)
 })
-function seri (win: number, r: number, xm: number, ym: number, x: number[], y: number[]) {
-    serial.writeValue("win", win)
-    for (let Index = 0; Index <= x.length - 1; Index++) {
-        serial.writeValue("x", x[Index])
-        serial.writeValue("y", y[Index])
-    }
-}
 input.onButtonPressed(Button.AB, function () {
-    r = 31
-    xm = 64
-    ym = 32
-    w = 360 / 5 * 2
-    win = 0
-    x = [64]
-    y = [1]
-    seri(win, r, xm, ym, x, y)
-    win = w - 90
-    x.push(Math.floor(xm - Math.sin(win) * r))
-    y.push(Math.floor(ym - Math.cos(win) * r))
-    seri(win, r, xm, ym, x, y)
-    oled_z.oled_Line1(x[0], y[0], x[1], y[1])
-    win = 2 * w + win
-    x.push(Math.floor(xm + Math.sin(win) * r))
-    y.push(Math.floor(ym + Math.cos(win) * r))
-    seri(win, r, xm, ym, x, y)
-    oled_z.oled_Line1(x[1], y[1], x[2], [0][2])
-    basic.pause(100)
+    kitronik_VIEW128x64.clear()
+    x = [
+    64,
+    82,
+    34,
+    94,
+    46,
+    64
+    ]
+    y = [
+    0,
+    57,
+    22,
+    22,
+    57,
+    0
+    ]
+    xarr = x
+    yarr = y
+    oled_z.znullp(10, 0)
+    // macheEtwas()
+    oled_z.zlinie(xarr.length)
 })
 input.onButtonPressed(Button.B, function () {
     oled_z.oled_Line1(54, 32, 74, 32)
@@ -40,13 +36,10 @@ input.onButtonPressed(Button.B, function () {
     oled_z.oled_Line1(57, 25, 71, 39)
     oled_z.oled_Line1(57, 39, 71, 25)
 })
+let yarr: number[] = []
+let xarr: number[] = []
 let y: number[] = []
 let x: number[] = []
-let w = 0
-let ym = 0
-let xm = 0
-let r = 0
-let win = 0
 basic.showIcon(IconNames.Surprised)
 kitronik_VIEW128x64.clear()
-oled_z.Kreis(64, 32, 31)
+oled_z.oled_Kreis(64, 32, 31)
